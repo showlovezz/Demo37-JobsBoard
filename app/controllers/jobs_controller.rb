eac71d4bebe1243feb216f4dd150193d41793544
vita <dragic.hsieh@gmail.com>
2018-06-12 16:50:09 +0800

    Done jobs controller actions and pages

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,51 @@
+class JobsController < ApplicationController
+
+	before_action :find_job, only: [:show, :edit, :update, :destroy]
+	
+	def index
+		@jobs = Job.all
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(job_params)
+		if @job.save
+			redirect_to jobs_path, notice: "新工作建立成功"
+		else
+			render "new", alert: "是不是少寫了什麼?"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @job.update(job_params)
+			redirect_to jobs_path, notice: "工作修改成功"
+		else
+			render "edit", alert: "是不是改了不該改的?"
+		end
+	end
+
+	def destroy
+		@job.destroy
+		redirect_to jobs_path, alert: "此工作被刪除了"
+	end
+
+	private
+
+	def job_params
+		params.require(:job).permit(:title, :description, :company, :url, :address, :people, :work_location, :image)
+	end
+
+	def find_job
+		@job = Job.find(params[:id])
+	end
+
+end
